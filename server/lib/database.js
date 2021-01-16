26ea56e1c6c5c1e5a3aca2f9edea9108798d8233
Avinash <avinash.pakala93@gmail.com>
2021-01-16 21:57:33 +0530

    database update

--- server/lib/database.js
+++ server/lib/database.js
@@ -6,7 +6,7 @@ const monitor = require('pg-monitor')
 monitor.attach(pgpOptions)
 
 //initialize connectionString
-const connectionString = process.env.DATABASE_URL
+const connectionString = "postgres://rpprmquidwxsrf:d127584b59b71cbede81aafee4a8185dce1b33f09e6bc3c964478232fa5e7df1@ec2-18-235-107-171.compute-1.amazonaws.com:5432/ddls7rgkh9tse8?ssl=true"
 
 
 const db = pgp(connectionString)
